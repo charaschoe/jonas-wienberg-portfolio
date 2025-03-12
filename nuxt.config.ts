c40c349b887e35665ce95f247f03cd95496e540e
J W <jonas.wienberg@hfg-gmuend.de>
2025-03-12 14:28:00 +0100

    Refactor Nuxt configuration to use @nuxtjs/tailwindcss module, update compatibility date, and restructure CSS imports for Tailwind. Remove package-lock.json and add devDependencies for Tailwind and PostCSS.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/main.css"],
+  compatibilityDate: "2025-03-12",
 });
